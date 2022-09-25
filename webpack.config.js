2807161a6d10a9c59527790a8e7e606d7c8d9e60
Bimarsha Khanal <khanalbimarsha7@gmail.com>
2022-09-25 06:35:29 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+       {
+          test: /\.css$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "postcss-loader",
+              ],
+        },
+        ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebPackPlugin({ template: './src/index.html' })
+  ],
+}
